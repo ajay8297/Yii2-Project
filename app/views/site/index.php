9e5e163807cbee3d7f2c4b0012eaf9af79a9cb0c
ajay8297 <145636793+ajay8297@users.noreply.github.com>
2025-04-29 12:16:46 +0530

    Create index.php

---
+++ app/views/site/index.php
@@ -0,0 +1,9 @@
+<?php
+use yii\helpers\Html;
+
+$this->title = 'Welcome to Yii2 Demo';
+?>
+<div class="site-index">
+    <h1><?= Html::encode($this->title) ?></h1>
+    <p>This is a minimal Yii2 application deployed using Docker Swarm!</p>
+</div>
