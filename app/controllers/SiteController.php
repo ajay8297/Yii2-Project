a2c9a955506f618754b2cea2a2276eace1d4c6b1
ajay8297 <145636793+ajay8297@users.noreply.github.com>
2025-04-29 12:17:24 +0530

    Update SiteController.php

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -1,42 +1,12 @@
 <?php
-$params = require __DIR__ . '/params.php';
-$db = require __DIR__ . '/db.php';
+namespace app\controllers;
 
-$config = [
-    'id' => 'basic',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'components' => [
-        'request' => [
-            'cookieValidationKey' => 'your-secret-key',
-        ],
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'db' => $db,
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [],
-        ],
-    ],
-    'params' => $params,
-];
+use yii\web\Controller;
 
-return $config;
+class SiteController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
